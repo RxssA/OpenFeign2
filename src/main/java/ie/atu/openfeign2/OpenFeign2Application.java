a5336772e72740319bfdb536e3129d0db13c6c74
G00401655@atu.ie <G00401655@atu.ie>
2023-10-11 14:47:47 +0100

    push1

---
+++ src/main/java/ie/atu/openfeign2/OpenFeign2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.openfeign2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeign2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeign2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/openfeign2/OpenFeign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.openfeign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeign2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
